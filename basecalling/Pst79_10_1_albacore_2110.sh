4446eb46c9b723a13ca662eceb79ca2e53b50441
Anjuni Peiris <ap5514@raijin5.(none)>
2018-02-13 14:18:06 +1100

    Now we fixed the JOBFS to $PBS_JOBFS and added some timing functions to the main basecalling in the script.

--- basecalling/Pst79_10_1_albacore_2110.sh
+++ basecalling/Pst79_10_1_albacore_2110.sh
@@ -4,13 +4,15 @@
 #PBS -l walltime=24:00:00,mem=126GB,ncpus=16
 #PBS -l jobfs=400GB
 
+set -vx
+
 #define the input and output directories
 INPUT=/short/sd34/ap5514/raw_data/Pst79_10_1_zipped
 OUTPUT=/short/sd34/ap5514/basecalling/basecalled_albacore2110/Pst79_10_1
 
 #move data over to JOBFS
 
-cd $JOBFS
+cd $PBS_JOBFS
 mkdir in
 mkdir out_1d
 mkdir out_1d2
@@ -30,21 +32,23 @@ rm *.tar.gz
 #now do the basecalling 1D basecalling
 module load albacore/2.1.10
 
-cd $JOBFS
-read_fast5_basecaller.py -i in -t 36 -c r95_450bps_linear.cfg -s out_1d -r -o fastq -n 0 -q 0 --disable_pings
+cd $PBS_JOBFS
+time read_fast5_basecaller.py -i in -t 36 -c r95_450bps_linear.cfg -s out_1d -r -o fastq -n 0 -q 0 --disable_pings
 
-tar czf Pst79_10_1_zipped_1d_albacore2110.tar.gz out_1d
+time tar czf Pst79_10_1_zipped_1d_albacore2110.tar.gz out_1d
 
 mv Pst79_10_1_zipped_1d_albacore2110.tar.gz $OUTPUT
 
+rm -r out_1d
 
 #now do the 1d2 basecalling
-full_1dsq_basecaller.py -i in -t 20 -c r95_450bps_linear_for_1dsq.cfg -s out_1d2 -r -o fastq -n 0 -q 0 --disable_pings
+time full_1dsq_basecaller.py -i in -t 20 -c r95_450bps_linear_for_1dsq.cfg -s out_1d2 -r -o fastq -n 0 -q 0 --disable_pings
 
-tar czf Pst79_10_1_zipped_1d2_albacore2110.tar.gz out_1d2
+time tar czf Pst79_10_1_zipped_1d2_albacore2110.tar.gz out_1d2
 
 mv Pst79_10_1_zipped_1d2_albacore2110.tar.gz $OUTPUT
-   
+
+rm out_1d2   
 rm -r in
 
 
