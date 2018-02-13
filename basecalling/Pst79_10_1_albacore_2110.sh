0e53b6c7c17a3bb169c3ff052e45aba0d419a868
Anjuni Peiris <ap5514@raijin3.(none)>
2018-02-14 10:04:05 +1100

    We removed the 1d2 basecalling. We made a seqrun variable so the scirpt is easier to adapt.

--- basecalling/Pst79_10_1_albacore_2110.sh
+++ basecalling/Pst79_10_1_albacore_2110.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #PBS -P sd34
-#PBS -q express 
-#PBS -l walltime=24:00:00,mem=126GB,ncpus=16
-#PBS -l jobfs=400GB
+#PBS -q normal 
+#PBS -l walltime=30:00:00,mem=126GB,ncpus=16
+#PBS -l jobfs=250GB
 
 set -vx
 
 #define the input and output directories
-INPUT=/short/sd34/ap5514/raw_data/Pst79_10_1_zipped
-OUTPUT=/short/sd34/ap5514/basecalling/basecalled_albacore2110/Pst79_10_1
+seqrun=Pst79_10_1
+INPUT=/short/sd34/ap5514/raw_data/${seqrun}_zipped
+OUTPUT=/short/sd34/ap5514/basecalling/basecalled_albacore2110/${seqrun}
 
 #move data over to JOBFS
 
@@ -35,20 +36,12 @@ module load albacore/2.1.10
 cd $PBS_JOBFS
 time read_fast5_basecaller.py -i in -t 36 -c r95_450bps_linear.cfg -s out_1d -r -o fastq -n 0 -q 0 --disable_pings
 
-time tar czf Pst79_10_1_zipped_1d_albacore2110.tar.gz out_1d
+time tar czf ${seqrun}_1d_albacore2110.tar.gz out_1d
 
-mv Pst79_10_1_zipped_1d_albacore2110.tar.gz $OUTPUT
+mv ${seqrun}_1d_albacore2110.tar.gz $OUTPUT
 
 rm -r out_1d
 
-#now do the 1d2 basecalling
-time full_1dsq_basecaller.py -i in -t 20 -c r95_450bps_linear_for_1dsq.cfg -s out_1d2 -r -o fastq -n 0 -q 0 --disable_pings
-
-time tar czf Pst79_10_1_zipped_1d2_albacore2110.tar.gz out_1d2
-
-mv Pst79_10_1_zipped_1d2_albacore2110.tar.gz $OUTPUT
-
-rm out_1d2   
 rm -r in
 
 
