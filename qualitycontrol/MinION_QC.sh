62fc9867caacc72b6232b2ec938c08a10a382692
Anjuni Peiris <ap5514@raijin1.(none)>
2018-02-16 16:45:10 +1100

    Changed variables to NCI account name, removed basecalling script and changed general script.

--- qualitycontrol/MinION_QC.sh
+++ qualitycontrol/MinION_QC.sh
@@ -10,7 +10,7 @@ set -vx
 
 #define some variables at the start
 #give this job a name
-name=Pst79_run1_1d2_0245
+name=Pst79_run1-4_1d
 ###
 INPUT=$short/
 OUTPUT=$short/
@@ -43,38 +43,45 @@ cp ${ASSEMBLY_BASE_FOLDER}/${genome_file} GENOME/.
 cd TAR_FILES
 for x in *.tar.gz
 do
+len=${#x}
+folder= ${x::len-7}
+mv ${x} ${folder}/.
+cd ${folder} 
 tar -xopf ${x}&
+cd $PBS_JOBFS/TAR_FILES
 done
 
 wait
 
 rm *.tar.gz
 
+
+#now caputre all the fastq for pass and fail seperately plus the summary file
 cd $PBS_JOBFS
-mkdir albacore_fastq
-cp albacore_output/sequencing_summary.txt albacore_fastq/.
-cat albacore_output/workspace/*.fastq > albacore_fastq/${name}.all.fastq
+
+mkdir albacore_fastq 
+cd albacore_fastq
+
+cat TAR_FILES/*/out_1d/workspace/fail/*.fastq > ${name}_fail.fastq
+cat TAR_FILES/*/out_1d/workspace/pass/*.fastq > ${name}_pass.fastq
+cat TAR_FILES/*/out_1d/sequencing_summary.txt  > ${name}_sequencing_summary.txt
 
 #remove TAR_FILES and zip up stuff
+cd $PBS_JOBS
 rm -r TAR_FILES
-tar -cvzf albacore_output.tar.gz albacore_output
-rm -r albacore_output
-
-#now move everything from this step down already
-mv albacore_output.tar.gz ${OUTPUT}/.
-
-cp -r albacore_fastq ${OUTPUT}/.
+tar -cvzf ${name}_albacore_output.tar.gz albacore_output
+mv ${name}_albacore_output.tar.gz ${OUTPUT}/.
 
 #quickly check we are in the right spot
 #run minion qc script
 # minion QC
 echo "Running MinION QC R script"
 outmqc=$PBS_JOBFS/"minionQC"
-seqsum=$PBS_JOBFS/albacore_fastq/"sequencing_summary.txt"
+seqsum=$PBS_JOBFS/albacore_fastq/${name}_sequencing_summary.txt
 
 
 #just in case the original minin_QC.R script gets changed we port these changes
-rsync -P ${LANFEAR_SCRIPTS}/minion_qc/minion_QC.R minion_QC.R
+rsync -P ${LANFEAR_SCRIPTS}/minion_QC.R minion_QC.R
 
 module load R/3.4.0
 mkdir $outmqc
@@ -88,43 +95,46 @@ mkdir $outngmlr
 cd $outngmlr
 echo "Mapping with ngmlr"
 date
-time ~/myapps/ngmlr/0.2.6/ngmlr/bin/ngmlr-0.2.6/ngmlr -t ${threads} -r ${PBS_JOBFS}/GENOME/${genome_file} -q ${PBS_JOBFS}/albacore_fastq/${name}.all.fastq -o ${name}.ngmlr.out.sam -x ont
+
+time /home/106/ap5514/myapps/ngmlr/bin/ngmlr-0.2.6/ngmlr -t ${threads} -r ${PBS_JOBFS}/GENOME/${genome_file} -q ${PBS_JOBFS}/albacore_fastq/${name}_pass.fastq -o ${name}_pass.ngmlr.out.sam -x ont
+time /home/106/ap5514/myapps/ngmlr/bin/ngmlr-0.2.6/ngmlr -t ${threads} -r ${PBS_JOBFS}/GENOME/${genome_file} -q ${PBS_JOBFS}/albacore_fastq/${name}_fail.fastq -o ${name}_fail.ngmlr.out.sam -x ont
 echo "Done Mapping with ngmlr"
 date
 
 
 #mv all the ngmlr stats analysis
 #now make a bam file out of it
-module load samtools/1.4
-samtools view -bS -@ $threads ${name}.ngmlr.out.sam > ${name}.out.bam
-samtools sort -@ $threads ${name}.out.bam -o ${name}.out.bam
-samtools index ${name}.out.bam
 
-rm ${name}.ngmlr.out.sam
+FIX THIS
+module load samtools/1.4
+
 
 module load java/jdk1.8.0_60
-#now do the qualimap
-/home/106/rn5305/downloads/qualimap_v2.2.1/qualimap bamqc -bam ${name}.out.bam -outdir ${PBS_JOBFS}/"qualimap_all/" -nt $threads -c --java-mem-size=$mem_size
-/home/106/rn5305/downloads/qualimap_v2.2.1/qualimap bamqc -bam ${name}.out.bam -outdir ${PBS_JOBFS}/"qualimap_gff/" -gff ${PBS_JOBFS}/GENOME/${gff_file} -nt $threads -c --java-mem-size=$mem_size
-
-
-# run nanoplot
-#module to load for nanoplot
-module unload albacore/1.2.6
-module load nanoplot/0.16.2
-
+module load nanoplot/1.0.0
 outnano=${PBS_JOBFS}/"nanoplot/"
-NanoPlot --fastq_rich ${PBS_JOBFS}/${name}.all.fastq --outdir $outnano --threads $threads --loglength
-NanoPlot --bam ${name}.out.bam --outdir $outnano --threads $threads --loglength --prefix bam
+NanoPlot --fastq_rich ${PBS_JOBFS}/albacore_fastq/${name}_pass.fastq --outdir $outnano --threads $threads --loglength
+NanoPlot --fastq_rich ${PBS_JOBFS}/albacore_fastq/${name}_fail.fastq --outdir $outnano --threads $threads --loglength
+
+for x in *.sam
+do
+samtools view -bS -@ $threads ${x} > ${x}.bam
+samtools sort -@ $threads ${x}.bam -o ${x}.out.bam
+samtools index ${x}.out.bam
+/home/106/ap5514/myapps/qualimap_v2.2.1/qualimap bamqc -bam ${x}.out.bam -outdir ${PBS_JOBFS}/"qualimap_all/" -nt $threads -c --java-mem-size=$mem_size
+/home/106/ap5514/myapps/qualimap_v2.2.1/qualimap bamqc -bam ${x}.out.bam -outdir ${PBS_JOBFS}/"qualimap_gff/" -gff ${PBS_JOBFS}/GENOME/${gff_file} -nt $threads -c --java-mem-size=$mem_size
+rm ${x}
+NanoPlot --bam ${x}.out.bam --outdir $outnano --threads $threads --loglength --prefix bam
 
 # stats on reads > various length (thanks to @gringer here: https://bioinformatics.stackexchange.com/questions/678/get-the-mapping-statistics-of-a-single-read-$
-outbam=$outngmlr/${name}.out.bam
+outbam=${x}.out.bam
 samtools view -h $outbam |     awk -F'\t' '{if((/^@/) || (length($10)>1000)){print $0}}' |  samtools stats   | grep '^SN' | cut -f 2- > stats_1k.txt
 samtools view -h $outbam |     awk -F'\t' '{if((/^@/) || (length($10)>2000)){print $0}}' |  samtools stats   | grep '^SN' | cut -f 2- > stats_2k.txt
 samtools view -h $outbam |     awk -F'\t' '{if((/^@/) || (length($10)>10000)){print $0}}' | samtools stats   | grep '^SN' | cut -f 2- > stats_10k.txt
 samtools view -h $outbam |     awk -F'\t' '{if((/^@/) || (length($10)>20000)){print $0}}' | samtools stats   | grep '^SN' | cut -f 2- > stats_20k.txt
 samtools view -h $outbam |     awk -F'\t' '{if((/^@/) || (length($10)>100000)){print $0}}' | samtools stats  | grep '^SN' | cut -f 2- > stats_100k.txt
 samtools view -h $outbam |     awk -F'\t' '{if((/^@/) || (length($10)>200000)){print $0}}' | samtools stats  | grep '^SN' | cut -f 2- > stats_200k.txt
+done
+
 
 #now ngmlr is done go on with minimap
 
@@ -137,43 +147,38 @@ cd $outminimap2
 echo "Mapping with minimap2"
 date
 time 
-~/myapps/minimap2/minimap2/minimap2 -ax map10k ${PBS_JOBFS}/albacore_fastq/${name}.all.fastq ${PBS_JOBFS}/GENOME/${genome_file} > ${name}.minimap2.out.sam
+~/myapps/minimap2/minimap2/minimap2 -ax ava-ont ${PBS_JOBFS}/albacore_fastq/${name}_pass.fastq ${PBS_JOBFS}/GENOME/${genome_file} > ${name}_pass.minimap2.out.sam
+~/myapps/minimap2/minimap2/minimap2 -ax ava-ont ${PBS_JOBFS}/albacore_fastq/${name}_fail.fastq ${PBS_JOBFS}/GENOME/${genome_file} > ${name}_fail.minimap2.out.sam
 echo "Done mapping with minimap2"
 date
 
 
-###Now process the minmap2 sam file
-#now make a bam file out of it
-samtools view -bS -@ $threads ${name}.minimap2.out.sam > ${name}.minimap2.out.bam
-samtools sort -@ $threads ${name}.minimap2.out.bam -o ${name}.minimap2.out.bam
-samtools index ${name}.minimap2.out.bam
 
-rm ${name}.minimap2.out.sam
-
-#now do the qualimap
-/home/106/rn5305/downloads/qualimap_v2.2.1/qualimap bamqc -bam ${name}.minimap2.out.bam -outdir ${PBS_JOBFS}/"minimpa2.qualimap_all/" -nt $threads -c --java-mem-size=$mem_size
-
-/home/106/rn5305/downloads/qualimap_v2.2.1/qualimap bamqc -bam ${name}.minimap2.out.bam -outdir ${PBS_JOBFS}/"minimap2.qualimap_gff/" -gff ${PBS_JOBFS}/GENOME/${gff_file} -nt $threads -c --java-mem-size=$mem_size
-
-#run nanoplot
-#module to load for nanoplot
-module load nanoplot
-
-#NanoPlot --bam ${name}.minimap2.out.bam --outdir $outnano --threads $threads --loglength --prefix bam
+for x in *.sam
+do
+samtools view -bS -@ $threads ${x} > ${x}.bam
+samtools sort -@ $threads ${x}.bam -o ${x}.out.bam
+samtools index ${x}.out.bam
+/home/106/ap5514/myapps/qualimap_v2.2.1/qualimap bamqc -bam ${x}.out.bam -outdir ${PBS_JOBFS}/"qualimap_all/" -nt $threads -c --java-mem-size=$mem_size
+/home/106/ap5514/myapps/qualimap_v2.2.1/qualimap bamqc -bam ${x}.out.bam -outdir ${PBS_JOBFS}/"qualimap_gff/" -gff ${PBS_JOBFS}/GENOME/${gff_file} -nt $threads -c --java-mem-size=$mem_size
+rm ${x}
+NanoPlot --bam ${x}.out.bam --outdir $outnano --threads $threads --loglength --prefix bam
 
 # stats on reads > various length (thanks to @gringer here: https://bioinformatics.stackexchange.com/questions/678/get-the-mapping-statistics-of-a-single-read-$
-outbam=$outminimap2/${name}.minimap2.out.bam
+outbam=${x}.out.bam
 samtools view -h $outbam |     awk -F'\t' '{if((/^@/) || (length($10)>1000)){print $0}}' |  samtools stats   | grep '^SN' | cut -f 2- > stats_1k.txt
 samtools view -h $outbam |     awk -F'\t' '{if((/^@/) || (length($10)>2000)){print $0}}' |  samtools stats   | grep '^SN' | cut -f 2- > stats_2k.txt
 samtools view -h $outbam |     awk -F'\t' '{if((/^@/) || (length($10)>10000)){print $0}}' | samtools stats   | grep '^SN' | cut -f 2- > stats_10k.txt
 samtools view -h $outbam |     awk -F'\t' '{if((/^@/) || (length($10)>20000)){print $0}}' | samtools stats   | grep '^SN' | cut -f 2- > stats_20k.txt
 samtools view -h $outbam |     awk -F'\t' '{if((/^@/) || (length($10)>100000)){print $0}}' | samtools stats  | grep '^SN' | cut -f 2- > stats_100k.txt
 samtools view -h $outbam |     awk -F'\t' '{if((/^@/) || (length($10)>200000)){print $0}}' | samtools stats  | grep '^SN' | cut -f 2- > stats_200k.txt
-
-
-
+done
 #now move everything back to normal
+
 cd ${PBS_JOBFS}
-mv * ${OUTPUT}/.
+
+cp $outmqc ${OUTPUT}/.
+cp $outminimap2 ${OUTPUT}/.
+cp $outngmlr ${OUTPUT}/.
 
 
