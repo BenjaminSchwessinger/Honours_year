da337517a3ace63e457a5396a27f9e3597361d2c
Anjuni Peiris <ap5514@raijin4.(none)>
2018-04-22 22:03:33 +1000

    Fixed input file name. 22/04/2018.

--- nci/basecalling/fast5_Pst79_3a.sh
+++ nci/basecalling/fast5_Pst79_3a.sh
@@ -8,7 +8,7 @@ set -vx
 
 #define the input and output directories
 
-seqrun=Pst79_3a
+seqrun=Pst79_3
 INPUT=/short/sd34/ap5514/raw_data/${seqrun}_zipped
 OUTPUT=/short/sd34/ap5514/basecalling/basecalled_albacore2110/${seqrun}
 
@@ -45,7 +45,7 @@ module load albacore/2.1.10
 cd $PBS_JOBFS
 time read_fast5_basecaller.py -i in -t 36 -c r94_450bps_linear.cfg -s out_1d -r -o fastq,fast5 -n 0 -q 0 --disable_pings
 
-time tar -czf $OUTPUT/${seqrun}_1d_albacore2110.tar.gz out_1d
+time tar -czf $OUTPUT/Pst79_3a_1d_albacore2110.tar.gz out_1d
 
 rm -r out_1d
 
